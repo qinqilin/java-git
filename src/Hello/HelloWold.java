324b4d6d957e581bd704f08b2833a5658aef5700
qql <163.com>
2021-11-23 10:17:43 +0800

    final

--- src/Hello/HelloWold.java
+++ src/Hello/HelloWold.java
@@ -4,5 +4,6 @@ public class HelloWold {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("hello world!");
+        System.out.println("hello world!");
     }
 }
